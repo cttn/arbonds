50d881a9301379599370b899f925a7273f94cdae
Socaz <socaz.dev@gmail.com>
2020-09-23 13:04:41 -0300

    Added summary dataframe

--- bonds.py
+++ bonds.py
@@ -3,32 +3,37 @@ import pandas as pd
 import numpy as np
 from scipy.optimize import fsolve
 from price_scrapper import PScrapper
+
 #To Do:
 # * Discard cash flows with negative time intervals
 # * Add comission option.
 # * Improve file handle
+# * Improve handling of coupon periods in mdur
+# * Improve handling of "D" kind and names.
 
 class Arbonds:
 
-    names = ["AL29","AL30","AL35","AE38","AL41"]
+    names = ["AL29", "AL30", "AL35", "AE38", "AL41"]
 
     def __init__(self):
-        self.prices = None
+        self.prices = {}
         self.date = None
         self.bond_cfs = {}
         self.bond_irr = {}
         self.macaulay = {}
         self.bond_mdur = {}
+        self.summary = None
         
-    def update(self, site="rava",dkind=True):
-        self.set_bond_mdur(site=site,dkind=dkind)
+    def update(self, site="rava", dkind=True):
+        self.set_bond_prices(site=site, dkind=dkind)
+        self.set_summary(site=site, dkind=dkind)
 
-    def read_csv(self,arch):
+    def read_csv(self, arch):
         """Reads bond interest and principal payments"""
         path = "data/" + arch
-        df = pd.read_csv(path, names=['fecha','i','A'])
-        df['fecha']=pd.to_datetime(df.fecha,format="%d/%m/%y")
-        df.set_index("fecha",inplace=True)
+        df = pd.read_csv(path, names=['fecha', 'i', 'A'])
+        df['fecha']=pd.to_datetime(df.fecha, format="%d/%m/%y")
+        df.set_index("fecha", inplace=True)
         return df
         
     def npv(self, i_rate, cash_flows, years):
@@ -56,11 +61,11 @@ class Arbonds:
         since first one"""
         years_raw = df.index.to_series().subtract(df.index[0]).values
         years_raw = years_raw/(60*60*24*365)
-        return np.array([i*1e-9 for i in years_raw.astype(float)])
+        return np.array([i * 1e-9 for i in years_raw.astype(float)])
 
-    def get_cfs_array(self,bond_df):
+    def get_cfs_array(self, bond_df):
         """Given a bond dataframe, returns the implied array of cashflows"""
-        return (bond_df.i+bond_df.A).values
+        return (bond_df.i + bond_df.A).values
         
     def compute_irr(self, cash_flows, years, x0):
         """Computes IRR for given cash_flows"""
@@ -84,52 +89,71 @@ class Arbonds:
             else:
                 ticker_list = self.names
         ps = PScrapper()
-        vals = ps.scrap(site,ticker_list)
-        self.prices = vals
+        val_dict = ps.scrap(site, ticker_list)
+        if dkind:
+            for k, v in val_dict.items():
+                self.prices[k[:-1]] = v
+        else:
+            self.prices = val_dict
         self.date = dt.now()
     
-    def set_bond_cfs(self,site,dkind=True,log=True):
+    def set_bond_cfs(self, site, dkind=True, log=True):
         """Si no es especie D, debo dolarizar los precios"""
         if log:
             print("Getting prices from " + str(site))
-        self.set_bond_prices(site=site,dkind=dkind)
+        self.set_bond_prices(site=site, dkind=dkind)
         
         if log:
             print("Computing cashflows")
-        for i in range(len(self.names)):
-            ticker = self.names[i]
-            price = self.prices[i]
+        for ticker in self.names:
+            price = self.prices[ticker]
             df_raw = self.read_csv(ticker + ".DAT")
-            df = self.include_purchase_cfs(df_raw,0,-price)
+            df = self.include_purchase_cfs(df_raw, 0, -price)
             self.bond_cfs[ticker] = df
         
-    def set_bond_irr(self,site="rava",dkind=True):
+    def set_bond_irr(self, site="rava", dkind=True):
         """For each bond, calculates irr using info in self.bond_cfs"""
         if self.bond_cfs == {}:
-            self.set_bond_cfs(site=site,dkind=dkind)
+            self.set_bond_cfs(site=site, dkind=dkind)
         for ticker in self.names:
             self.bond_irr[ticker] = self.get_bond_irr(self.bond_cfs[ticker])
             
-    def get_macaulay_dur(self,bond_df,irr):
+    def get_macaulay_dur(self, bond_df, irr):
         cfs = self.get_cfs_array(bond_df)
         yrs = self.get_yrs_array(bond_df)
-        npv = self.npv(irr,cfs[1:],yrs[1:])
+        npv = self.npv(irr,cfs[1:], yrs[1:])
         acum = 0
         for i in range(len(cfs)):
-            acum = acum + self.npv(irr,cfs[i],yrs[i])*yrs[i]
+            acum = acum + self.npv(irr, cfs[i], yrs[i])*yrs[i]
         return acum/npv
         
-    def set_bond_macaulay(self,site="rava",dkind=True):
+    def set_bond_macaulay(self, site="rava", dkind=True):
         if self.bond_irr == {}:
-            self.set_bond_irr(site=site,dkind=dkind)
+            self.set_bond_irr(site=site, dkind=dkind)
         for ticker in self.names:
             bdf = self.bond_cfs[ticker]
             irr = self.bond_irr[ticker]
-            self.macaulay[ticker] = self.get_macaulay_dur(bdf,irr)
-        
-    def set_bond_mdur(self,site="rava",dkind=True,periods=2.0):
+            self.macaulay[ticker] = self.get_macaulay_dur(bdf, irr)
+
+    def set_bond_mdur(self, site="rava", dkind=True, periods=2.0):
         if self.macaulay == {}:
-            self.set_bond_macaulay(site=site,dkind=dkind)
+            self.set_bond_macaulay(site=site, dkind=dkind)
         for ticker in self.names:
-            self.bond_mdur[ticker] = self.macaulay[ticker]/(1 + 
+            self.bond_mdur[ticker] = self.macaulay[ticker] / (1 + 
                 self.bond_irr[ticker]/periods)
+
+    def set_summary(self, site, dkind=True):
+        """Creates a dataframe with summary information and stores it
+        in self.summary dataframe"""
+        if self.bond_mdur == {}:
+            self.set_bond_mdur(site=site, dkind=dkind)
+        summary = {}
+        for ticker in self.names:
+            summary[ticker]=[self.date,
+                             self.prices[ticker],
+                             self.bond_irr[ticker],
+                             self.bond_mdur[ticker]]
+        df = pd.DataFrame(summary).T
+        df.columns = ["date", "price", "IRR", "MDur"]
+        self.summary = df
+
--- price_scrapper.py
+++ price_scrapper.py
@@ -13,17 +13,17 @@ class PScrapper:
     def __init__(self):
         self.htmlparser = etree.HTMLParser()
         
-    def scrap(self,site,list_tickers):
-        results = []
+    def scrap(self, site, list_tickers):
+        results = {}
         for ticker in list_tickers:
             response = urlopen(self.site[site]['url'] + ticker)
             tree = etree.parse(response, self.htmlparser)
             res = tree.xpath(self.site[site]['xpath'])
-            value_raw = res[self.site[site]['loc']].replace(",",".")
-            results.append(self.myfloat(value_raw))
+            value_raw = res[self.site[site]['loc']].replace(",", ".")
+            results[ticker] = self.myfloat(value_raw)
         return results
         
-    def myfloat(self,value):
+    def myfloat(self, value):
         """Encapsulates np.float"""
         try:
             myfloat = np.float(value)
