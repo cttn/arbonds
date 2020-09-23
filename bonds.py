acb39ffa5f5cb55b423ad24f3b2b2e54da070e53
Socaz <socaz.dev@gmail.com>
2020-09-23 11:29:31 -0300

    More code cleanup

--- bonds.py
+++ bonds.py
@@ -4,9 +4,9 @@ import numpy as np
 from scipy.optimize import fsolve
 from price_scrapper import PScrapper
 #To Do:
-# Ver cómo manejar intérvalos de tiempo negativos, que debería descartar
-# del cálculo
-# Agregar comision
+# * Discard cash flows with negative time intervals
+# * Add comission option.
+# * Improve file handle
 
 class Arbonds:
 
@@ -14,13 +14,14 @@ class Arbonds:
 
     def __init__(self):
         self.prices = None
+        self.date = None
         self.bond_cfs = {}
         self.bond_irr = {}
         self.macaulay = {}
         self.bond_mdur = {}
         
-    def update(self, site="rava",especie_D=True):
-        self.update_bond_mdur(site,especie_D)
+    def update(self, site="rava",dkind=True):
+        self.set_bond_mdur(site=site,dkind=dkind)
 
     def read_csv(self,arch):
         """Reads bond interest and principal payments"""
@@ -35,9 +36,9 @@ class Arbonds:
         an interest rate i_rate. cash_flwos and years must be numpy arrays"""
         return np.sum(cash_flows / ( 1 + i_rate ) ** years)
     
-    def include_purchase_cash_flow(self, bond_df, i, R, date=None):
+    def include_purchase_cfs(self, bond_df, i, R, date=None):
         """Completes the bond cash flows with purchase data. This method
-        insert an interest i and principal R at given date. If date is None,
+        inserts an interest i and principal R at given date. If date is None,
         then date = datetime.now()"""
         
         #  If not given, set date to now
@@ -59,7 +60,7 @@ class Arbonds:
 
     def get_cfs_array(self,bond_df):
         """Given a bond dataframe, returns the implied array of cashflows"""
-        return cfs = (bond_df.i+bond_df.A).values
+        return (bond_df.i+bond_df.A).values
         
     def compute_irr(self, cash_flows, years, x0):
         """Computes IRR for given cash_flows"""
@@ -67,71 +68,68 @@ class Arbonds:
                                   x0=x0, 
                                   args=(cash_flows, years)))
     
-    def get_bond_irr(self, bond_df):
-        """Given a dataframe of bond cashflows, cumpute the implied irr"""
+    def get_bond_irr(self, bond_df, guess=0.10):
+        """Given a dataframe of bond cashflows, cumpute the implied irr.
+        'guess' is the initial irr guess for the iteration"""
         cfs = self.get_cfs_array(bond_df)
         yrs = self.get_yrs_array(bond_df)
-        return self.compute_irr(cfs,yrs,0.10)
+        return self.compute_irr(cfs, yrs, guess)
     
-    def get_bond_prices(self, site, ticker_list=None, dkind=True):
-        """Returns prices of bond taken from site using PScrapper.
+    def set_bond_prices(self, site, ticker_list=None, dkind=True):
+        """Returns prices of bond taken from 'site' using PScrapper.
         if dkind, then append "D" to tickers"""
         if ticker_list is None:
-            if append_D:
+            if dkind:
                 ticker_list = [i + "D" for i in self.names]
             else:
                 ticker_list = self.names
         ps = PScrapper()
-        values = ps.scrap(site,ticker_list)
-        self.prices = values
-        return None
+        vals = ps.scrap(site,ticker_list)
+        self.prices = vals
+        self.date = dt.now()
     
-    def update_bond_cfs(self,site,especie_D=True,log=True):
+    def set_bond_cfs(self,site,dkind=True,log=True):
         """Si no es especie D, debo dolarizar los precios"""
         if log:
-            print("Obteniendo precios desde " + str(site))
-        self.get_prices(site)
+            print("Getting prices from " + str(site))
+        self.set_bond_prices(site=site,dkind=dkind)
         
         if log:
-            print("Calculando cashflows")
+            print("Computing cashflows")
         for i in range(len(self.names)):
             ticker = self.names[i]
             price = self.prices[i]
             df_raw = self.read_csv(ticker + ".DAT")
-            df = self.true_cash_flows(df_raw,0,-price)
+            df = self.include_purchase_cfs(df_raw,0,-price)
             self.bond_cfs[ticker] = df
-        return None
         
-    def update_bond_irr(self,site="rava",especie_D=True):
+    def set_bond_irr(self,site="rava",dkind=True):
         """For each bond, calculates irr using info in self.bond_cfs"""
         if self.bond_cfs == {}:
-            self.update_bond_cfs(site,especie_D=especie_D)
+            self.set_bond_cfs(site=site,dkind=dkind)
         for ticker in self.names:
             self.bond_irr[ticker] = self.get_bond_irr(self.bond_cfs[ticker])
-        return None
             
     def get_macaulay_dur(self,bond_df,irr):
-        cfs = (bond_df.i+bond_df.A).values
-        yrs = self.years_diff(bond_df)
+        cfs = self.get_cfs_array(bond_df)
+        yrs = self.get_yrs_array(bond_df)
         npv = self.npv(irr,cfs[1:],yrs[1:])
         acum = 0
         for i in range(len(cfs)):
             acum = acum + self.npv(irr,cfs[i],yrs[i])*yrs[i]
         return acum/npv
         
-    def update_bond_macaulay(self,site="rava",especie_D=True):
+    def set_bond_macaulay(self,site="rava",dkind=True):
         if self.bond_irr == {}:
-            self.update_bond_irr(site,especie_D)
+            self.set_bond_irr(site=site,dkind=dkind)
         for ticker in self.names:
             bdf = self.bond_cfs[ticker]
             irr = self.bond_irr[ticker]
             self.macaulay[ticker] = self.get_macaulay_dur(bdf,irr)
-        return None
         
-    def update_bond_mdur(self,site="rava",especie_D=True,periods=2.0):
+    def set_bond_mdur(self,site="rava",dkind=True,periods=2.0):
         if self.macaulay == {}:
-            self.update_bond_macaulay(site,especie_D)
+            self.set_bond_macaulay(site=site,dkind=dkind)
         for ticker in self.names:
             self.bond_mdur[ticker] = self.macaulay[ticker]/(1 + 
                 self.bond_irr[ticker]/periods)
-        return None
